d317b0b6ebdd1e103038bbdee32233dd571d7c0d
Pawel <pawel@MacBook-Pro-Pawel.local>
2020-05-19 12:37:17 +0200

    adding planets actions tests

--- src/actions/__tests__/vehiclesActions.test.js
+++ src/actions/__tests__/vehiclesActions.test.js
@@ -1,4 +1,20 @@
 import * as actions from '../../actions/';
+import { cleanup } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+import swapi from '../../apis/swapi';
+import MockAdapter from 'axios-mock-adapter';
+
+let mockAdapter;
+
+beforeEach(() => {
+  mockAdapter = new MockAdapter(swapi);
+});
+
+afterEach(() => {
+  cleanup();
+  mockAdapter = null;
+});
 
 describe('Vehicle actions', () => {
   it(`${actions.setVehicles.name} has correct type`, () => {
@@ -23,5 +39,52 @@ describe('Vehicle actions', () => {
     const action = actions.setVehiclesCachedData();
 
     expect(action.type).toEqual(actions.SET_VEHICLES_CACHED_DATA);
+  });
+
+  it(`${actions.fetchVehicles.name} dispatches correct actions`, async () => {
+    const responseMock = [
+      {
+        name: 'X-wing',
+        speed: 6,
+      },
+    ];
+
+    mockAdapter.onGet(swapi.baseUrl).replyOnce(200, {
+      results: responseMock,
+    });
+
+    const mockStore = configureStore([thunkMiddleware]);
+    const store = mockStore({});
+
+    await store.dispatch(actions.fetchVehicles()).then(() => {
+      const storeActions = store.getActions();
+
+      expect(storeActions[0]).toEqual(actions.setError(null));
+      expect(storeActions[1]).toEqual(actions.setBusy(true));
+      expect(storeActions[2]).toEqual(actions.setPage(1));
+      expect(storeActions[3]).toEqual(actions.setVehicles(responseMock));
+      expect(storeActions[4]).toEqual(actions.setBusy(false));
+    });
+  });
+
+  it(`${actions.fetchVehicle.name} dispatches correct actions`, async () => {
+    const responseMock = {
+      name: 'Alderan',
+      population: 10000000,
+    };
+
+    mockAdapter.onGet(swapi.baseUrl).replyOnce(200, responseMock);
+
+    const mockStore = configureStore([thunkMiddleware]);
+    const store = mockStore({});
+
+    await store.dispatch(actions.fetchVehicle(1)).then(() => {
+      const storeActions = store.getActions();
+
+      expect(storeActions[0]).toEqual(actions.setError(null));
+      expect(storeActions[1]).toEqual(actions.setBusy(true));
+      expect(storeActions[2]).toEqual(actions.setVehicle(responseMock));
+      expect(storeActions[3]).toEqual(actions.setBusy(false));
+    });
   });
 });
